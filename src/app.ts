e64cdab592d32f66c883092098df940602fa0848
ronina24 <roni24682@gmail.com>
2024-04-20 16:49:27 +0300

    render-deleted app.use(static)

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app: Express = express();
-app.use(express.static('dist'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const logPath = path.join(__dirname, "../logs", "http.log");

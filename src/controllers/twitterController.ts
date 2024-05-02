c05eccc1c1df963859c3ce1ce4da1b12fcc2baf7
ronina24 <roni24682@gmail.com>
2024-05-02 09:19:54 +0300

    get users by username

--- src/controllers/twitterController.ts
+++ src/controllers/twitterController.ts
@@ -1,7 +1,7 @@
 import { spawn } from 'child_process';
 import path from 'path';
 
-function get_user() {
+function get_user_me() {
     const basePath = path.join(process.cwd(), 'src', 'X-api');
     console.log(basePath)
     const absoluteScriptPath = path.join(basePath, 'get-users-me.py');
@@ -20,5 +20,24 @@ function get_user() {
     });
 }
 
+function get_user() {
+    const basePath = path.join(process.cwd(), 'src', 'X-api');
+    console.log(basePath)
+    const absoluteScriptPath = path.join(basePath, 'get-users-by-context.py');
+    const pythonProcess = spawn('python', [absoluteScriptPath]);
 
-export {get_user}
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+    });
+}
+
+
+export {get_user_me, get_user}

e0864dc9b8ddcf42cf2f00981974fa4522c30e38
ronina24 <roni24682@gmail.com>
2024-06-13 01:35:57 +0300

    get graph by id api

--- src/routes/structureRouter.ts
+++ src/routes/structureRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { handleFile,removeGraphs,editGraph} from "../controllers/structureController"
+import { handleFile,removeGraphs,editGraph,getGraph} from "../controllers/structureController"
 import multer from "multer";
 
 const upload = multer();
@@ -7,5 +7,6 @@ const graphRouter = express.Router();
 graphRouter.post("/",upload.single("file"),handleFile);
 graphRouter.delete("/", removeGraphs);
 graphRouter.put('/:_id', editGraph);
+graphRouter.get('/:_id', getGraph);
 
 export default graphRouter;
